e5d49eea78828ddbbfe2de4c5f100e81d63e50a0
2per2 <twopercentwo@gmail.com>
2024-07-05 04:49:39 +0900

    server setting

--- src/server.js
+++ src/server.js
@@ -1,6 +1,28 @@
-const httpServer = require("http");
-const WSServer = require("ws");
+const http = require("http");
+const { Server } = require("socket.io");
+//const { instrument } = require("@socket.io/admin-ui");
 const express = require("express");
 const app = express();
 
+
+/* Setting app */
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("port", process.env.PORT || 3000);
+app.use("/public", express.static(__dirname + "/public"));
+//app.get('/', (req, res) => { res.render('home') });
+
+
+/* Setting http server and socket.io */
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+wsServer.on("connection", (socket) => {
+	console.log("someone connected!");
+});
+
+
+/* Start server */
+httpServer.listen(port, () => {
+	console.log("start server");
+});
