7c0b8d6ab9b71f36f36eb104d1307db17d3d9335
2per2 <twopercentwo@gmail.com>
2024-07-17 01:10:22 +0900

    Edited reservation view

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("form-reservation").addEventListener('submit', (event) => {
         event.preventDefault();
         console.log(window.userId);
-            
+        /*
         fetch('/signup', {
             method: 'POST',
             headers: {
@@ -25,6 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch(error => {
             console.error('Error occured for creating your account', error);
-        });
+        });*/
     });
 });
