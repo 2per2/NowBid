a4e42c2c4705abd8ef17c254478163058711e827
2per2 <twopercentwo@gmail.com>
2024-07-17 01:26:18 +0900

    Added middleware before get into reservation upload page

--- src/routers/reservationRouter.js
+++ src/routers/reservationRouter.js
@@ -1,9 +1,10 @@
 const express = require('express'),
 	router = express.Router(),
-    reservationController = require("../controllers/reservationController")
+    reservationController = require("../controllers/reservationController"),
+    isAuthenticated = require("../middlewares/auth");
 
 router.get('/reservation', reservationController.handleGetReservation);
-router.get('/reservation/upload', (req, res) => {
+router.get('/reservation/upload', isAuthenticated, (req, res) => {
     res.render("reservations/createReservation");
 });
 router.post('/reservation/upload', (req, res) => {
