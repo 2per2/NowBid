96f249f510eb617a48ad3e074d1d4427fe529e94
twopercentwo <twopercentwo@gmail.com>
2024-07-21 01:39:03 +0900

    New feature: uploading img to user's directory

--- src/middlewares/multerConfig.js
+++ src/middlewares/multerConfig.js
@@ -1,14 +1,24 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 require("dotenv").config();
+
+const makeDir = (dir) => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true }); // ensure the directory is created recursively
+    }
+    return dir;
+}
 
 // Multer의 저장소 설정
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, process.env.STORAGE_PATH); // 파일이 저장될 디렉토리
+        const userDir = path.join(process.env.STORAGE_PATH, String(req.user.id)); // convert req.user.id to string and combine with STORAGE_PATH
+        makeDir(userDir); // create the user-specific directory
+        cb(null, userDir); // set the user-specific directory as the destination
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // 파일 이름 설정
+        cb(null, Date.now() + path.extname(file.originalname)); // set the file name
     }
 });
 
