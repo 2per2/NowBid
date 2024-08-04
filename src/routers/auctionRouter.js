86526e616fb1052c6ebb93ce47a0278bfb051021
twopercentwo <twopercentwo@gmail.com>
2024-08-05 00:48:53 +0900

    � Deleted a url: /auction/:roomName

--- src/routers/auctionRouter.js
+++ src/routers/auctionRouter.js
@@ -2,9 +2,6 @@ const express = require('express'),
 	router = express.Router();
 
 router.get('/auction', (req, res) => { res.render('auctions/auction')});
-router.get('/auction/:roomName', (req, res) => { 
-    const name = req.params.roomName;
-    res.send(`This is ${name} room`)
-});
+router.get('/auction/:id');
 
 module.exports = router;
