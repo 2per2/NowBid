79c479e0f2fe5e630211ad93479bd059467049bb
twopercentwo <twopercentwo@gmail.com>
2024-08-07 03:02:56 +0900

    � Fixed method of signup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,12 +2,18 @@ const authService = require("../services/authService");
 
 exports.handleCreateUser = async (req, res, next) => {
     try {
-        const newUser = req.body;
-        const exUser = await authService.findUserByEmail(newUser.email);
+        const { username, email, password } = req.body;
+        const newUser = {
+            username: username,
+            email: email,
+            password: password
+        };
+        const exUser = await authService.findUserByEmail(email);
 
         if (exUser) {
             return res.status(409).json({ error: "사용 중인 이메일입니다." });
         }
+
         const createdUser = await authService.createUser(newUser);
         res.status(201).json(createdUser);
     } catch (error) {
